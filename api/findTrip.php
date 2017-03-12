d1045c7f6cbef34b8b38f146795c9460e1382966
Vincent PICOT <vincent1.picot@epitech.eu>
2017-03-12 12:24:44 +0100

    Remove debug

--- api/findTrip.php
+++ api/findTrip.php
@@ -6,7 +6,7 @@ include_once("timeTravel.php");
 include_once("findClosest.php");
 include_once("findBetterTransport.php");
 
-/*if (!isset($_GET["longStart"]) || !isset($_GET["latStart"]))
+if (!isset($_GET["longStart"]) || !isset($_GET["latStart"]))
 {
     http_response_code(400);
     die();
@@ -19,10 +19,11 @@ if (!isset($_GET["longEnd"]) || !isset($_GET["latEnd"]))
 
 $startPoint = array('long' => $_GET["longStart"], 'lat' => $_GET["latStart"]);
 $endPoint = array('long' => $_GET["longEnd"], 'lat' => $_GET["latEnd"]);
-*/
 
+/*
 $startPoint = array('long' => 6.114977680291502, 'lat' => 49.60185748029151);
 $endPoint = array('long' => 6.144977680291502, 'lat' => 49.62185748029151);
+*/
 
 $startStation = getClosestStation($startPoint);
 $endStation = getClosestStation($endPoint);
